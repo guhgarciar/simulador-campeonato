19858ca44bed91c711fe0cce2b9b53c56b21a5f4
Gustavo Garcia <gustavo.garcia.if@gmail.com>
2026-07-01 16:37:31 -0300

    Adiciona lógica de partidas, pontos e cartões

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
-import random #biblioteca que gera os dados randômicos
+import random
 
 class Time:
-    # O __init__ é o construtor. É ele que prepara a "caixinha" do time na memória.
-    # O 'nome_do_time' é a única coisa que vamos passar de fora.
     def __init__ (self, nome_do_time: str):
         self.nome: str = nome_do_time
-
-        # Todos os outros atributos começam zerados porque o campeonato não começou!
-        self.pontos: int = 0
         self.vitorias: int = 0
         self.empates: int = 0
         self.derrotas: int = 0
         self.gols_pro: int = 0
-        self.gols_contra: int = 0
+        self.gols_sofridos: int = 0
         self.saldo: int = 0
-        self.cartoes_amarelos: int = 0
-        self.cartoes_vermelhos: int = 0
+        self.amarelo: int = 0
+        self.vermelho: int = 0
 
-nomes_dos_times = ["São Paulo", "Flamengo", "Palmeiras", "Santos", "Corinthians"]
+times_campeonato = ["Athletico-PR", "Atlético-MG", "Bahia", "Botafogo", "Chapecoense", "Corinthians", "Coritiba", "Cruzeiro", "Flamengo", "Fluminense", "Grêmio", "Internacional", "Mirassol", "Palmeiras", "Red Bull Bragantino", "Remo", "Santos", "São Paulo", "Vasco", "Vitória"]
+times_cadastro = []
 
-campeonato = []
-
-for i in nomes_dos_times:
-    # 1. Criamos o objeto 'Time' na memória usando o texto da vez (i)
+for i in times_campeonato:
     novo_time = Time(nome_do_time=i)
-    # 2. Guardamos esse objeto completo dentro da nossa lista do campeonato
-    campeonato.append(novo_time)
+    times_cadastro.append(novo_time)
 
-for i in campeonato:
-    print(i.nome)
+for i in range(len(times_cadastro)):
+    for j in range(len(times_cadastro)):
+        if i != j:
+            time_casa = times_cadastro[i]
+            time_visitante = times_cadastro[j]
+            gols_time_casa = int(input(f"Gols do time {time_casa.nome}: "))
+            gols_time_visitante = int(input(f"Gols do time {time_visitante.nome}: "))
+            if gols_time_casa > gols_time_visitante:
+                time_casa.pontos += 3
+                time_casa.vitorias += 1
+                time_visitante.derrotas += 1
+            elif gols_time_casa == gols_time_visitante:
+                time_casa.pontos += 1
+                time_casa.empates += 1
+                time_visitante.pontos += 1
+                time_visitante.empates += 1
+            else:
+                time_visitante.pontos += 3
+                time_casa.derrotas += 1
+                time_visitante.vitorias += 1
+            time_casa.gols_pro += gols_time_casa
+            time_casa.gols_sofridos += gols_time_visitante
+            time_casa.saldo = (time_casa.gols_pro - time_casa.gols_sofridos)
+            time_visitante.gols_pro += gols_time_visitante
+            time_visitante.gols_sofridos += gols_time_casa
+            time_visitante.saldo = (time_visitante.gols_pro - time_visitante.gols_sofridos)
+            teve_amarelo_casa = random.choice([True, False])
+            if teve_amarelo_casa == True:
+                time_casa.amarelo += random.randint(1, 5)
+            teve_vermelho_casa = random.randint(1, 10)
+            if teve_vermelho_casa == 1:
+                opcoes_vermelho = [1, 1, 1, 1, 1, 1, 1, 2, 2, 3]
+                time_casa.vermelho += random.choice(opcoes_vermelho)
+            teve_amarelo_visitante = random.choice([True, False])
+            if teve_amarelo_visitante == True:
+                time_visitante.amarelo += random.randint(1, 5)
+            teve_vermelho_visitante = random.randint(1, 10)
+            if teve_vermelho_visitante == 1:
+                opcoes_vermelho = [1, 1, 1, 1, 1, 1, 1, 2, 2, 3]
+                time_visitante.vermelho += random.choice(opcoes_vermelho)
