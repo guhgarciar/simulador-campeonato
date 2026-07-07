b52bf1a6a920da2fdd85b77501ba9df90a94b170
Gustavo Garcia <gustavo.garcia.if@gmail.com>
2026-07-06 21:23:26 -0300

    Cria a rota GET /times para listar a classificacao

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ times_cadastro = []
 for i in times_campeonato:
     novo_time = Time(nome_do_time=i)
     times_cadastro.append(novo_time)
+
+@app.get("/times")
+def obter_tabela():
+    tabela_organizada = sorted(times_cadastro, key = lambda time: time.pontos, reverse = True)
+    return tabela_organizada
